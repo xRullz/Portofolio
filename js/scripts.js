b500114653a2ab06ce851f947548dcd79c85c930
xRullz <akunpertamapublic@gmail.com>
2024-11-23 18:56:48 +0700

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 // Add interactivity if needed
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio Loaded Successfully');
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio Loaded Successfully");
+});
+
+document.querySelectorAll("a.nav-link").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
